45a03a6bc112f76448de72d5f9d6eadfa0611239
eduardoogliari <eduardo.ogliari@outlook.com>
2025-07-26 17:58:30 -0300

    ExpandableGroup agora recolhe ou exibe conteudo

--- src/components/ExpandableGroup.tsx
+++ src/components/ExpandableGroup.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
+
 export type ExpandableGroup = {
     title : string;
     children? : React.ReactNode;
 };
 
 export default function ExpandableGroup( props : ExpandableGroup ) {
+    const [showContents, setShowContents] = useState(true);
+
     return (
-        <>
-            <h3>{props.title}</h3>
-            {props.children}
-        </>
+        <div className="expandable-group">
+            <div className="expandable-group-header">
+                <span>{props.title}</span>
+                <button onClick={() => setShowContents( !showContents )}>{ showContents ? '-' : '+' }</button>
+            </div>
+            { (showContents)? props.children : <></> }
+        </div>
     );
 }
