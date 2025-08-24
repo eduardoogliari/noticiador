0a849ab73fb85dc925bd58c4ceddd020d9a5c924
eduardoogliari <eduardo.ogliari@outlook.com>
2025-08-24 00:05:04 -0300

    Adicionado botão para atualizar todos os feeds

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,11 +1,16 @@
 export type ToolbarProp = {
     onClickAddSubscription : () => void;
+    refreshAllFeeds: () => void;
 };
 
 export default function Toolbar( props : ToolbarProp ) {
     return (
         <div className="toolbar">
             <button className="toolbar-button" onClick={props.onClickAddSubscription}>➕ New Feed</button>
+            <span className='v-separator'></span>
+            <button onClick={props.refreshAllFeeds}>Refresh all</button>
+            {/* <button>Mark all as read</button> */}
+            {/* <button>Send feed bin</button> */}
         </div>
     );
 }
